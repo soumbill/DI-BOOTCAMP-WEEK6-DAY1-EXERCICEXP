67da21f93527da3710f4a40b6aefb1546c276add
soumbill <konesoumailabilly@gmail.com>
2023-01-30 21:13:27 +0100

    exerciceXP-D1 commit

---
+++ Exo2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo2 - PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            $my_variable = "Hello PHP";
+            echo $my_variable;
+        ?>
+    </h1>
+</body>
+</html>
---
+++ Exo3 & Exo4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo3 - PHP</title>
+</head>
+<body>
+    <?php 
+        $bonjour = "Bonjour";
+        $journee = "bonne journee";
+    ?>
+    <h1><?php echo " $bonjour passe une $journee "; ?></h1>
+    
+    <!-- Exo4 strrev fontion pour inverser une chaine de caractere -->
+    <h1><?php echo strrev("$bonjour passe une $journee"); ?></h1>
+
+</body>
+</html>
